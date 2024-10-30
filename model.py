5c0b43d5a12666de9279bf59c440bd0c0df68066
xinli2008 <czu_lixin@163.com>
2024-10-30 23:54:17 +0800

    update

--- model.py
+++ model.py
@@ -34,3 +34,99 @@ class PatchEmbedding(nn.Module):
         x = x.flatten(2).transpose(1, 2)
         x = self.norm(x)
         return x, h, w
+
+
+class PatchMerging(nn.Module):
+    """
+    2D Image Patch Merging Layer
+    """
+    def __init__(self, dim, norm_layer = nn.LayerNorm):
+        super(PatchEmbedding, self).__init__()
+        self.dim = dim
+        self.reduction = nn.Linear(4 * dim, 2 * dim, bias = False)
+        self.norm = norm_layer(4 * dim)
+
+    def forward(self, x, h, w):
+        b, l, c = x.shape
+        assert l == h * w, "input feature has wrong size"
+
+        x = x.view(b, h, w, c)
+
+        # NOTE: 如果输入的feature_map的宽高不是2的整数倍, 则需要padding。
+        pad_input = (h % 2 == 1) or (w % 2 == 1)
+        if pad_input:
+            # NOTE: x: [b, h, w, c], 我们需要从宽高两个维度上进行padding。
+            # (c_front, c_back, w_left, w_right, h_top, h_bottom)
+            x = F.pad(x, (0, 0, 0, w % 2, 0, h % 2))
+
+        # padding完之后, 需要进行merging操作
+        x0 = x[:, 0::2, 0::2, :]
+        x1 = x[:, 1::2, 0::2, :]
+        x2 = x[:, 0::2, 1::2, :]
+        x3 = x[:, 1::2, 1::2, :]
+        x = torch.cat([x0, x1, x2, x3], dim = -1)
+        x = x.view(b, -1, 4 * c)
+
+        x = self.norm(x)
+        x = self.reduction(x)
+
+        return x
+
+
+class SwinTransformer(nn.Module):
+    """
+    Swin Transformer Tiny Version
+    """
+    def __init__(self, patch_size = 4, in_channels = 3, num_classes = 1000,
+                embedding_dim = 96, depths = (2, 2, 6, 2), num_heads = (3, 6, 12, 24),
+                window_size = 7, mlp_ratio = 4, qkv_bias = True, drop_rate = 0.1, 
+                norm_layer = nn.LayerNorm, patch_norm = True):
+        self.num_classes = num_classes
+        self.num_layers = len(depths)
+        self.embedding_dim = embedding_dim
+        self.patch_norm = patch_norm
+        self.mlp_ratio = mlp_ratio
+        # NOTE: 每经过一个stage, num_features就会变大两倍。
+        self.num_features = int(embedding_dim * 2 ** (self.num_layers - 1))
+        
+        # patch-embed
+        self.patch_embedding = PatchEmbedding(patch_size, in_channels, embedding_dim, norm_layer)
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        # build-layers
+        self.layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            layers = nn.Sequential()
+            self.layers.append(layers)
+        
+        self.norm = norm_layer(self.num_features)
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+    
+    def forward(self, x):
+        """
+        Perform swin transformer forward process.
+        Args:
+            x: torch.Tensor, [b, c, h, w]
+        """
+        x, h, w = self.patch_embedding(x)
+        x = self.pos_drop(x)
+
+        for layer in self.layers:
+            x, h, w = layer(x, h, w)
+        
+        x = self.norm(x)  # [b, l, c]
+        x = self.avgpool(x.transpose(1, 2))  # [b, c, 1]
+        x = torch.flatten(x, 1)
+        x = self.head(x)
+        return x
