24ed7ce60c616d78b80c5e3a25225fb91148b13c
lxin123456 <71311087+xinli2008@users.noreply.github.com>
2024-11-02 20:02:45 +0800

    Update

--- model.py
+++ model.py
@@ -1,14 +1,15 @@
 import torch
+torch.set_printoptions(threshold=torch.inf)
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np 
 
 class PatchEmbedding(nn.Module):
     """
     2D Image Patch Embedding
     """
     def __init__(self, patch_size = 4, in_channels = 3, embedding_dim = 96, norm_layer = None):
-        super(self, PatchEmbedding).__init__()
+        super(PatchEmbedding, self).__init__()
         patch_size = (patch_size, patch_size)
         self.patch_size = patch_size
         self.in_channels = in_channels
@@ -41,7 +42,7 @@ class PatchMerging(nn.Module):
     2D Image Patch Merging Layer
     """
     def __init__(self, dim, norm_layer = nn.LayerNorm):
-        super(PatchEmbedding, self).__init__()
+        super(PatchMerging, self).__init__()
         self.dim = dim
         self.reduction = nn.Linear(4 * dim, 2 * dim, bias = False)
         self.norm = norm_layer(4 * dim)
@@ -73,13 +74,138 @@ class PatchMerging(nn.Module):
         return x
 
 
+def window_partition(x, window_size):
+    r"""
+    将feature_map按照window_size划分成没有重叠的window
+    Args:
+        x: torch.Tensor, [b, h, w, c]
+        window_size: int
+    Returns:
+        windows: [b * number_window, window_size, window_size, c]
+    """
+    b, h, w, c = x.shape     
+    # [b, h, w, c] -> [b, window_count_h, window_size, window_count_w, window_size, c]
+    x = x.view(b, h // window_size, window_size, w // window_size, window_size, c)
+    # permute: [b, window_count_h, window_count_w, window_size, window_size, c]
+    # view: [b * window_count_h * window_count_w, window_size, window_size, c]
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, c)
+    return windows
+
+
+def window_reverse(windows, window_size, h, w):
+    r"""
+    将一个个不重叠的window还原成之前的特征图
+    Args:
+        windows: torch.Tensor, [b * number_window, window_size, window_size, c]
+        window_size: int
+        h: int
+        w: int
+    Returns:
+        x: torch.Tensor, [b, h, w, c]
+    """
+    b = int(windows.shape[0] / ( h * w / window_size / window_size))
+    x = windows.view(b, h // window_size, w // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(b, h, w, -1)
+    return x
+
+
+class MLP(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features, act_layer = nn.GELU, drop_rate = 0.1):
+        super(MLP, self).__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_rate)
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop2 = nn.Dropout(drop_rate)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+    
+
+class WindowAttention(nn.Module):
+    def __init__(self,):
+        pass
+
+
 class SwinTransformerBlock(nn.Module):
     """
     swin transformer block
     """
-    def __init__(self, dim, num_heads, windows_size, shift_size, mlp_ratio, qkv_bias, drop_rate, norm_layer):
-        pass
+    def __init__(self, dim, num_heads, windows_size, shift_size, mlp_ratio, qkv_bias, drop_rate, act_layer, norm_layer):
+        super(SwinTransformerBlock, self).__init__()
+        self.dim = dim
+        self.num_heads = num_heads
+        self.window_size = windows_size
+        self.shift_size = shift_size
+        self.mlp_ratio = mlp_ratio
 
+        assert 0 <= self.shift_size <= self.window_size, "shift_size has a wrong size!"
+        self.norm1 = norm_layer(dim)
+        self.attn = WindowAttention()
+        self.dropout = nn.Dropout(drop_rate)
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = MLP(in_features = dim, hidden_features = mlp_hidden_dim, out_features = dim, act_layer = act_layer, drop_rate = drop_rate)
+    
+    def forward(self, x, attention_mask):
+        h, w = self.h, self.w
+        b, l, c = x.shape
+        assert l == h * w, "input features has wrong size"
+
+        shortcut = x
+        x = self.norm1(x)
+        x = x.view(b, h, w, c)
+
+        # NOTE:将特征图给padding成windows_size的整数倍, 方便于接下来将特征图给划分成不重叠的窗口
+        pad_l = pad_t = 0
+        pad_r = (self.window_size - w % self.window_size) % self.window_size
+        pad_b = (self.window_size - h % self.window_size) % self.window_size
+        x = F.pad(x, (0, 0, pad_l, pad_r, pad_t, pad_b))
+        _, Hp, Wp, _ = x.shape
+        
+        # NOTE:进行窗口平移
+        # 如果shift_size>0, 则需要将上面的某些行和左面的某些列进行移动。
+        # 将上面的shift_size行移动到下方, 将左侧的shift_size列移动到右边去。
+        if self.shift_size > 0:
+            shift_x = torch.roll(x, shifts=(-self.shift_size, -shift_x), dims = (1, 2))
+        else:
+            shift_x = x
+            attention_mask = None
+
+        # NOTE: 将特征图按窗口进行拆分
+        x_windows = window_partition(shift_x, self.window_size)  # [b * number_count_w * number_count_h, window_size, window_size, c]
+        x_windows = x_windows.view(-1, self.window_size * self.window_size)  # [b * number_count, window_size * window_size, c]
+
+        # NOTE: 窗口/移动窗口自注意力机制
+        attention_windows = self.attn(x_windows, mask = attention_mask)
+
+        # NOTE: 合并窗口
+        attention_windows = attention_windows.view(-1, self.window_size, self.window_size, c)
+        shifted_x = window_reverse(attention_windows, self.window_size, Hp, Wp)
+
+        # NOTE: 将经过移动的特征图回复原样
+        if self.shift_size > 0:
+            x = torch.roll(shifted_x, shifts = (self.shift_size, self.shift_size), dims = (1,2))
+        else:
+            x = shift_x
+
+        # NOTE: 将前面padding的数据给去掉
+        if pad_r > 0 or pad_b > 0:
+            x = x[:, :h, :w, :].contiguous()
+        
+        x = shortcut + self.dropout(x)
+        x = x + self.mlp(self.norm2(x))
+
+        return x
+    
 
 class BasicLayer(nn.Module):
     """
@@ -88,7 +214,7 @@ class BasicLayer(nn.Module):
     def __init__(self, dim, depth, num_heads, windows_size, mlp_ratio, qkv_bias, drop_rate, norm_layer, downsample):
         super().__init__()
         self.dim = dim
-        self.windows_size = windows_size
+        self.window_size = windows_size
         # NOTE: 
         self.shift_size = windows_size // 2
 
@@ -102,6 +228,7 @@ class BasicLayer(nn.Module):
                 mlp_ratio = mlp_ratio,
                 qkv_bias = qkv_bias,
                 drop_rate = drop_rate,
+                act_layer = nn.GELU, 
                 norm_layer = norm_layer
             )
          for i in range(depth)   
@@ -112,6 +239,56 @@ class BasicLayer(nn.Module):
             self.downsample = downsample(dim = dim, norm_layer = norm_layer)
         else:
             self.downsample = None
+    
+    def create_mask(self, x, h, w):
+        r"""
+        生成移动窗口自注意力机制所需要的attention mask。
+        这个mask的作用时在平移窗口中创建屏蔽区域, 从而更好的实现跨窗口信息交互。
+        """
+        # 计算Hp和Wp,确保它们是window_size的整数倍
+        Hp = int(np.ceil(h / self.window_size)) * self.window_size
+        Wp = int(np.ceil(w / self.window_size)) * self.window_size
+
+        # 创建image_mask, 并且为每个区域创建编号, 编号相同的为同一区域。
+        img_mask = torch.zeros((1, Hp, Wp, 1), device=x.device)
+        h_slices = (slice(0, -self.window_size),
+                    slice(-self.window_size, -self.shift_size),
+                    slice(-self.shift_size, None))
+        w_slices = (slice(0, -self.window_size),
+                    slice(-self.window_size, -self.shift_size),
+                    slice(-self.shift_size, None))
+
+        cnt = 0
+        for h in h_slices:
+            for w in w_slices:
+                img_mask[:, h, w, :] = cnt
+                cnt += 1
+
+        # 将img_mask分割成窗口并重塑为二维张量
+        mask_windows = window_partition(img_mask, self.window_size)  # [b * window_count_h * window_count_w, window_size, window_size, c]
+        mask_windows = mask_windows.view(-1, self.window_size * self.window_size) # [b * window_count_h * window_count_w * c, window_size * window_size]
+        
+        # NOTE: 通过对窗口内的编号进行差值计算, 生成注意力掩码。差值不为0的位置表示不同的窗口区域。
+        attention_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)
+
+        # NOTE: 将不同的区域的位置填充为-100, 表示在自注意力中抑制这些位置的影响;
+        # 将相同区域的位置填充为0, 表示正常的注意力。
+        attention_mask = attention_mask.masked_fill(attention_mask!=0, float(-100.0)).masked_fill(attention_mask==0, float(0.0))
+        
+        return attention_mask
+
+    def forward(self, x, h, w):
+        attention_mask = self.create_mask(x, h, w)
+        
+        for block in self.blocks:
+            x = block(x, attention_mask)
+        
+        # NOTE: 经过每一个stage后, 后面会接一个patch_merging操作来降低图像的分辨率
+        if self.downsample is not None:
+            x = self.downsample(x, h, w)
+            h, w = (h + 1) // 2, (w + 1) // 2
+        
+        return x, h, w
 
 
 class SwinTransformerTiny(nn.Module):
@@ -122,6 +299,7 @@ class SwinTransformerTiny(nn.Module):
                 embedding_dim = 96, depths = (2, 2, 6, 2), num_heads = (3, 6, 12, 24),
                 window_size = 7, mlp_ratio = 4, qkv_bias = True, drop_rate = 0.1, 
                 norm_layer = nn.LayerNorm, patch_norm = True):
+        super(SwinTransformerTiny, self).__init__()
         self.num_classes = num_classes
         self.num_layers = len(depths)
         self.embedding_dim = embedding_dim
@@ -141,7 +319,7 @@ class SwinTransformerTiny(nn.Module):
                 dim = int(embedding_dim * 2 ** i_layer),
                 depth = depths[i_layer],
                 num_heads = num_heads[i_layer],
-                window_size = window_size,
+                windows_size = window_size,
                 mlp_ratio = mlp_ratio,
                 qkv_bias = qkv_bias,
                 drop_rate = drop_rate,
@@ -153,7 +331,7 @@ class SwinTransformerTiny(nn.Module):
         self.norm = norm_layer(self.num_features)
         self.avgpool = nn.AdaptiveAvgPool1d(1)
         self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
-        self.apply(self._init_weights)
+        # self.apply(self._init_weights)
     
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
@@ -170,14 +348,21 @@ class SwinTransformerTiny(nn.Module):
         Args:
             x: torch.Tensor, [b, c, h, w]
         """
-        x, h, w = self.patch_embedding(x)
-        x = self.pos_drop(x)
+        x, h, w = self.patch_embedding(x)   # [b, c, h, w] -> [b, l, c]
+        x = self.pos_drop(x)                # [b, l, c]
 
         for layer in self.layers:
             x, h, w = layer(x, h, w)
         
-        x = self.norm(x)  # [b, l, c]
-        x = self.avgpool(x.transpose(1, 2))  # [b, c, 1]
-        x = torch.flatten(x, 1)
-        x = self.head(x)
+        x = self.norm(x)                    # [b, l, c]
+        x = self.avgpool(x.transpose(1, 2)) # [b, c, 1]
+        x = torch.flatten(x, 1)             # [b, c]
+        x = self.head(x)                    # [b, 1]
         return x
+
+
+if __name__ == "__main__":
+    input_tensor = torch.randn([1, 3, 224, 224])
+    model = SwinTransformerTiny()
+    output_tensor = model(input_tensor)
+    print(output_tensor.shape)
